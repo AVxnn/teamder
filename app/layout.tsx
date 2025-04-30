10397d10ccd9b5ed88909d98c3596d1de11a3ca6
George Romashko <george@thevann.ru>
2025-05-01 01:46:08 +0300

    fix: script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import SessionProviderWrapper from './providers/SessionProviderWrapper';
+import Head from 'next/head';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script async src="https://web.telegram.org/js/telegram-web-app.js" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
